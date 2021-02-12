d53d6c76b2da6a7484cd417fa7bf9d94cd1eee84
Adalberto Garcia Garces <atiestorage@gmail.com>
2021-02-12 14:23:01 -0300

    Add files via upload

---
+++ Unit1.dfm
@@ -0,0 +1,133 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  BorderStyle = bsSingle
+  Caption = 'Troca nomes'
+  ClientHeight = 607
+  ClientWidth = 586
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object DirectoryListBox1: TDirectoryListBox
+    Left = 0
+    Top = 180
+    Width = 281
+    Height = 364
+    Align = alLeft
+    FileList = FileListBox1
+    TabOrder = 0
+  end
+  object FileListBox1: TFileListBox
+    Left = 299
+    Top = 180
+    Width = 287
+    Height = 364
+    Align = alRight
+    ItemHeight = 13
+    TabOrder = 1
+  end
+  object Panel1: TPanel
+    Left = 0
+    Top = 0
+    Width = 586
+    Height = 161
+    Align = alTop
+    Alignment = taLeftJustify
+    Caption = 'Panel1'
+    ShowCaption = False
+    TabOrder = 2
+    ExplicitTop = -6
+    object Label1: TLabel
+      Left = 280
+      Top = 16
+      Width = 65
+      Height = 13
+      Caption = 'Saida vai ser:'
+    end
+    object LabeledEdit1: TLabeledEdit
+      Left = 14
+      Top = 35
+      Width = 251
+      Height = 21
+      EditLabel.Width = 143
+      EditLabel.Height = 13
+      EditLabel.Caption = 'Nome de arquivos para trocar'
+      TabOrder = 0
+      Text = 'Arquivo_numero_'
+      OnChange = LabeledEdit1Change
+    end
+    object LabeledEdit2: TLabeledEdit
+      Left = 14
+      Top = 79
+      Width = 251
+      Height = 21
+      EditLabel.Width = 153
+      EditLabel.Height = 13
+      EditLabel.Caption = 'Colocar numero a intervalos de:'
+      NumbersOnly = True
+      TabOrder = 1
+      Text = '5'
+      OnChange = LabeledEdit2Change
+    end
+    object CheckBox1: TCheckBox
+      Left = 14
+      Top = 129
+      Width = 155
+      Height = 17
+      Caption = 'Come'#231'ar numera'#231#227'o em 0'
+      TabOrder = 2
+      OnClick = CheckBox1Click
+    end
+    object Memo1: TMemo
+      Left = 271
+      Top = 35
+      Width = 281
+      Height = 111
+      Color = clBtnFace
+      ReadOnly = True
+      ScrollBars = ssVertical
+      TabOrder = 3
+    end
+  end
+  object Panel2: TPanel
+    Left = 0
+    Top = 544
+    Width = 586
+    Height = 63
+    Align = alBottom
+    TabOrder = 3
+    object Button1: TButton
+      Left = 46
+      Top = 22
+      Width = 219
+      Height = 25
+      Caption = 'Trocar nomes na pasta seleccionada'
+      TabOrder = 0
+      OnClick = Button1Click
+    end
+    object Button2: TButton
+      Left = 376
+      Top = 22
+      Width = 113
+      Height = 25
+      Caption = 'Fechar'
+      TabOrder = 1
+      OnClick = Button2Click
+    end
+  end
+  object DriveComboBox1: TDriveComboBox
+    Left = 0
+    Top = 161
+    Width = 586
+    Height = 19
+    Align = alTop
+    DirList = DirectoryListBox1
+    TabOrder = 4
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,180 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.FileCtrl;
+
+type
+  TForm1 = class(TForm)
+    DirectoryListBox1: TDirectoryListBox;
+    FileListBox1: TFileListBox;
+    Button1: TButton;
+    Button2: TButton;
+    LabeledEdit1: TLabeledEdit;
+    LabeledEdit2: TLabeledEdit;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    DriveComboBox1: TDriveComboBox;
+    CheckBox1: TCheckBox;
+    Label1: TLabel;
+    Memo1: TMemo;
+    procedure Button2Click(Sender: TObject);
+    function exemplo(nome:string; num:integer):string;
+    function troca_nomes(nome:string; num:integer; totalfiles:integer):string;
+    function cant0(lengtstring:integer):string;
+    procedure LabeledEdit1Change(Sender: TObject);
+    procedure LabeledEdit2Change(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure CheckBox1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+case MessageDlg('Você tem certeza absoluta de executar a troca de nomes? Esta ação não e reversível, tenha precaução', mtConfirmation, [mbOK, mbCancel], 0) of
+  mrOk:
+    begin
+troca_nomes(LabeledEdit1.Text,strtoint(LabeledEdit2.text),FileListBox1.Count-1);
+Memo1.SelStart :=0;
+Memo1.SelLength:=1;
+
+FileListBox1.Update;
+    end;
+  mrCancel:
+    begin
+    // Write code here for pressing button Cancel
+    ShowMessage('Revisa tudo bem antes de confirmar.');
+    end;
+end;
+
+
+
+
+
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+application.Terminate
+end;
+
+procedure TForm1.CheckBox1Click(Sender: TObject);
+begin
+exemplo(LabeledEdit1.Text,strtoint(LabeledEdit2.text));
+Memo1.SelStart :=0;
+Memo1.SelLength:=1;
+end;
+
+procedure TForm1.LabeledEdit1Change(Sender: TObject);
+begin
+exemplo(LabeledEdit1.Text,strtoint(LabeledEdit2.text));
+Memo1.SelStart :=0;
+Memo1.SelLength:=1;
+end;
+
+procedure TForm1.LabeledEdit2Change(Sender: TObject);
+begin
+ try
+if strtoint(LabeledEdit2.text)>0 then
+ begin
+ exemplo(LabeledEdit1.Text,strtoint(LabeledEdit2.text));
+ end;
+ except
+  ;
+ end;
+  Memo1.SelStart :=0;
+Memo1.SelLength:=1;
+end;
+
+function TForm1.exemplo(nome:string; num:integer):string;
+var
+  I, N, lengtstring :Integer;
+  cant00:string;
+begin
+form1.Memo1.Lines.Clear;
+ I := 1;
+if form1.CheckBox1.Checked then
+begin
+ I := 0;
+end;
+   // aqui vc inicializa de quanto será o passo
+
+  N := 10;
+  while I <= N do
+  begin
+  lengtstring := Length(inttostr(N))- Length(inttostr(I));
+  cant00:=cant0(lengtstring);
+    form1.Memo1.Lines.Add(nome + cant00 + inttostr(I))  ;
+
+
+    Inc(I, num);
+  end;
+end;
+
+
+
+ function TForm1.troca_nomes(nome:string; num:integer; totalfiles:integer):string;
+var
+  I, N, movec,lengtstring :Integer;
+  cant00, ext:string;
+begin
+form1.Memo1.Lines.Clear;
+ I := 1;
+if form1.CheckBox1.Checked then
+begin
+ I := 0;
+end;
+   // aqui vc inicializa de quanto será o passo
+  movec:=0;
+  N := (num*totalfiles)+I;
+
+  cant00:='';
+
+  while I <= N do
+  begin
+  lengtstring := Length(inttostr(N))- Length(inttostr(I));
+  cant00:=cant0(lengtstring);
+  ext:= ExtractFileExt(FileListBox1.Items.Strings[movec]);
+ form1.Memo1.Lines.Add(nome + cant00 + inttostr(I) + '.' + ext)  ;
+  if not RenameFile(FileListBox1.Items.Strings[movec],nome + cant00 + inttostr(I) + '.' + ext) then
+   showmessage('Error renaming file!');
+     movec:=movec+1;
+    Inc(I, num);
+  end;
+end;
+
+
+   function TForm1.cant0(lengtstring:integer):string;
+   var
+   x:integer;
+   cant0:string;
+   begin
+   cant0 :='';
+   for x := 0 to lengtstring -1 do
+   begin
+   cant0 := cant0 + '0';
+   end;
+   Result := cant0;
+   end;
+
+
+
+
+
+
+
+end.
